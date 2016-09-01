3e3cd3a995821ee4caa98070bc25f7b43077f749
Sascha Toennies <sascha.toennies@datenschutz.ekd.de>
2016-09-01 17:26:53 +0200

    First commit

---
+++ src/main/java/eu/toennies/javahttpobservatoryapi/Api.java
@@ -0,0 +1,17 @@
+package eu.toennies.javahttpobservatoryapi;
+
+/**
+ * Java-SSLLabs-API
+ * 
+ * This Java library provides basic access to Mozilla's Observatory API and is build upon
+ * the official API documentation at
+ * https://github.com/mozilla/http-observatory/blob/master/httpobs/docs/api.md
+ * 
+ * @author Sascha Tönnies <https://github.com/stoennies>
+ * @license GNU GENERAL PUBLIC LICENSE v3
+ */
+public class Api {
+	private static final String API_URL = "https://http-observatory.security.mozilla.org/api/v1";
+	private static final String VERSION = "0.0.1-SNAPSHOT";
+
+}
---
+++ src/main/java/eu/toennies/javahttpobservatoryapi/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 
+ * @author Sascha Tönnies <https://github.com/stoennies>
+ * @license GNU GENERAL PUBLIC LICENSE v3 *
+ */
+package eu.toennies.javahttpobservatoryapi;
